ee6b57fc5f6d3178bc01070a880e2d0250dcfc9f
Kevin <maketheworldwise@gmail.com>
2021-12-16 00:43:33 +0900

    :tada: First Commit

---
+++ src/main/java/com/example/csvspringprocessing/CsvSpringProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvspringprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvSpringProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvSpringProcessingApplication.class, args);
+    }
+
+}
