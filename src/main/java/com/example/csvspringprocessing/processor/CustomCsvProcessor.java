d71c4096ae495e8d50a9dbf946e29e014a3f9c05
Kevin <maketheworldwise@gmail.com>
2021-12-17 01:54:20 +0900

    :sparkles: openCSV 의존성 추가

---
+++ src/main/java/com/example/csvspringprocessing/processor/CustomCsvProcessor.java
@@ -0,0 +1,10 @@
+package com.example.csvspringprocessing;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CustomCsvProcessor {
+
+    private final MyProperties myProperties;
+
+}
